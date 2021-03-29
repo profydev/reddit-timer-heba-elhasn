773f7073cfc6eefbc3dc64cfee296f7b0a0d7ea7
heba-elhasn <newheba47@gmail.com>
2021-03-29 23:00:19 +0200

    1st task
    Fonts added , styles rest

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import WebFont from 'webfontloader';
+
 import App from './App';
+import 'normalize.css';
+
+// import './fonts/Bitter-Regular.ttf'
+
+WebFont.load({
+  google: {
+    families: ['Bitter', 'serif', 'Montserrat', 'sans-serif'],
+  },
+});
 
 // eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(<App />, document.getElementById('root'));
