e77aa9f624ba06d2ce54bd25371e72da99c25e58
heba-elhasn <newheba47@gmail.com>
2021-03-31 22:33:01 +0200

    Header component started
    new Header component implemented

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-import React from 'react';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Header from './Header';
 
-function App() {
+export default function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/Search">Search</Link>
+            </li>
+            
+          </ul>
+        </nav>
+        <Header/>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/Search">
+            <Search />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Search() {
+  return <h2>Search</h2>;
+}
+
